42229e29d958507371c594baacd29838bd93c003
brand <brandonnguyenn27@gmail.com>
2024-06-30 16:35:56 -0700

    add Link to header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,12 @@
 import Search from "./Search";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="bg-blue-500 p-3 flex items-center">
-      <p className="text-3xl text-white ml-4 mr-16">RiftTracker</p>
+      <Link className="text-3xl text-white ml-4 mr-16" href="/">
+        RiftTracker
+      </Link>
       <Search />
     </div>
   );
