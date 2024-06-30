cc404231bf63156352834c37f68e27bfe64ac9d9
brand <brandonnguyenn27@gmail.com>
2024-06-29 23:35:00 -0700

    types...

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Search from "./components/Search";
 export default function Home() {
   return (
-    <div>
+    <div className="m-2">
       <Search />
     </div>
   );
