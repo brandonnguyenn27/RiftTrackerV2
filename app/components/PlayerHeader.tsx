fd7046d7b050568c1ba20e41cf5b79b9793b6a11
brand <brandonnguyenn27@gmail.com>
2024-07-02 17:36:19 -0700

    idk whats going on but i hate frontend

---
+++ app/components/PlayerHeader.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import { Participant } from "../types/types";
+const PlayerHeader = (playerInfo: Participant) => {
+  const iconId = playerInfo.profileIcon;
+  return (
+    <div className="flex items-center bg-gray-600">
+      <Image
+        src="/favicon.ico"
+        width={100}
+        height={100}
+        alt="player icon"
+        className="rounded-xl m-6"
+      />
+      <span className="text-3xl font-semibold text-white">
+        {playerInfo.riotIdGameName}
+      </span>
+      <span className="text-2xl font-light m-1 text-gray-300 ">
+        #{playerInfo.riotIdTagline}
+      </span>
+    </div>
+  );
+};
+
+export default PlayerHeader;
--- app/search/[summonerName]/page.tsx
+++ app/search/[summonerName]/page.tsx
@@ -3,19 +3,27 @@ import { useParams } from "next/navigation";
 import { UsePlayerData } from "@/app/hooks/usePlayerData";
 import Header from "@/app/components/Header";
 import GameAccordion from "@/app/components/GameAccordion";
+import PlayerHeader from "@/app/components/PlayerHeader";
 
 export default function PlayerPage() {
   const summonerName = useParams().summonerName;
   const { playerData, loading, error, puuid } = UsePlayerData(
     summonerName as string
   );
+
   console.log(playerData);
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
   if (!playerData) return <div>No data found</div>;
+
+  const matchInfo = playerData.matchHistory[0];
+  const playerIndex = matchInfo.metadata.participants.indexOf(puuid);
+  const currentPlayerInfo = matchInfo.info.participants[playerIndex];
+
   return (
     <div>
       <Header />
+      <PlayerHeader {...currentPlayerInfo} />
       <div className="mt-8 flex justify-center">
         <GameAccordion
           gameData={playerData.matchHistory[0]}
